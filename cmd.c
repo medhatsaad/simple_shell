6c4a325cec16e5f411f68538b5f6631162abd748
Medhat Saad <medhat.saad0@gmail.com>
2023-05-22 18:37:48 +0300

    Add files via upload

---
+++ cmd.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+int main()
+{
+	char *cmd = NULL, **av = NULL;
+	size_t n = 0;
+	int ac;
+
+       	while (1)
+	{
+		print("$ ");
+
+		if (getline(&cmd, &n, stdin) == -1)
+		{
+			print("\n");
+			free(cmd);
+			exit(0);
+		}
+
+		ac = getac(cmd);
+		av = getav(cmd, ac, av);
+		start_proc(av);
+
+	}
+	exit(0);
+}
---
+++ process_exe.c
@@ -0,0 +1,55 @@
+#include "shell.h"
+
+/**
+ * start_proc - starts a new process if a executable is found
+ * @av: array of pointers
+ */
+
+void start_proc(char **av)
+{
+	pid_t proc;
+	char *fullpath;
+
+	if (av != NULL)
+	{
+		fullpath = _which(av);
+		
+		if (!_strcmp(fullpath, "1"))
+		{
+			if (fullpath != NULL)
+			{	av[0] = _strcp(fullpath);
+				free(fullpath);
+				proc = fork();
+				if (proc == 0)
+				{
+					if (execve(av[0], av, NULL) == -1)
+					{
+						perror("Error:");
+						exit(-1);
+					}
+				} else if (proc == -1)
+					perror("Failed to fork");
+				else
+				{
+					wait(NULL);
+				}
+			} else
+				perror("command not found");
+		}
+	}
+}
+/**
+ * print - prints a buffer to the standard output
+ * @buff: pointer to the buffer to print
+ */
+void print(char *buff)
+{
+	size_t n = 0;
+
+	if(buff != NULL)
+	{
+		n = strlen(buff);
+		write(1, buff, n);
+	}
+}
+
